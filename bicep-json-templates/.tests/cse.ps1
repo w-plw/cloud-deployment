6adb4c9775d8f58d23bd99a6ae2fa5ab2937b2b0
w-plw <108727963+w-plw@users.noreply.github.com>
2024-09-21 01:12:08 +0200

    cse script update

--- bicep-json-templates/.tests/cse.ps1
+++ bicep-json-templates/.tests/cse.ps1
@@ -4,7 +4,7 @@
 # Install IIS
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 # Download web files
-Invoke-WebRequest https://raw.githubusercontent.com/w-plw/cloud-deployment/refs/heads/main/.tests/index.html -OutFile C:\inetpub\wwwroot\index.htm
+Invoke-WebRequest https://raw.githubusercontent.com/w-plw/cloud-deployment/refs/heads/main/bicep-json-templates/.tests/index.html -OutFile C:\inetpub\wwwroot\index.htm
 
 
 
