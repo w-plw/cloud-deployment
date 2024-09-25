d913e31105c2670e1f6c85279d22e442a95ba037
w-plw <108727963+w-plw@users.noreply.github.com>
2024-09-25 16:38:50 +0200

    Update ISS_index_refresh.ps1

--- bicep-json-templates/.tests/ISS_index_refresh.ps1
+++ bicep-json-templates/.tests/ISS_index_refresh.ps1
@@ -2,6 +2,7 @@ $bootTime = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $mach
 $upTime = New-TimeSpan -Start $bootTime
 $upTimeminutes = [int]$upTime.TotalMinutes
 
+# update uptime time in index.html
 $file = 'C:\inetpub\wwwroot\index.html'
 $content = Get-Content -Path $file -Raw
 $pattern = 'uptime: \d+ minutes'
@@ -9,7 +10,18 @@ $replacement = "uptime: $upTimeminutes minutes"
 $content = $content -replace $pattern, $replacement
 Set-Content -Path $file -Value $content
 
-
-
+# update task presence in index.html
+$srvIISwwwPath = GetPhysicalPath "Default Web Site"
+if (Get-ScheduledTask $SchedTaskName) {
+    $srvIISwwwTask = "present"
+}
+else {
+    $srvIISwwwTask = "none"
+}
+$content = Get-Content -Path $file -Raw
+$pattern = 'IIS website update task: .+'
+$replacement = "IIS website update task: $srvIISwwwTask"
+$content = $content -replace $pattern, $replacement
+Set-Content -Path $file -Value $content
 
 
